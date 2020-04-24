48b51f99e2bcc406e9aab1fdfb11ef8734dafbd9
Alex Wang <ipoopfool@gmail.com>
2020-04-24 11:43:43 -0700

    get data to display in a sorted order

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { LayerAndTypeCounts } from "../../layerCounter";
+import { LayerAndTypeCounts, macroCaseToTitleCase } from "../../layerCounter";
 import "../styles/figma-plugin-ds.min.css";
 import "../styles/ui.css";
 
@@ -14,13 +14,36 @@ function useMessageListenerEffect(setState) {
 const defaultState: LayerAndTypeCounts = { layerCount: 0, typeCounts: {} };
 
 const App = ({}) => {
-  const [layerAndTypeCounts, setLayerAndTypeCounts] = React.useState(
+  const [{ layerCount, typeCounts }, setLayerAndTypeCounts] = React.useState(
     defaultState
   );
 
   useMessageListenerEffect(setLayerAndTypeCounts);
 
-  return <div>{JSON.stringify(layerAndTypeCounts)}</div>;
+  const blah = Object.entries(typeCounts).sort(
+    ([aKey, aValue], [bKey, bValue]) => {
+      // Sort by layer count descending
+      if (bValue !== aValue) {
+        return bValue - aValue;
+      }
+
+      // Fall back on sorting alphabetically
+      return aKey.localeCompare(bKey);
+    }
+  );
+
+  return (
+    <div>
+      <div>Selected Layers: {layerCount}</div>
+      <div>
+        {blah.map(([type, count]) => (
+          <div key={type}>
+            {macroCaseToTitleCase(type)}: {count}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
