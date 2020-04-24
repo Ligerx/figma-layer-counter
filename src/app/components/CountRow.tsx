7912d3f8c9d5b2f0e0abedaa39bfe70a6d75e3c0
Alex Wang <ipoopfool@gmail.com>
2020-04-24 13:28:39 -0700

    get icons mostly working.

--- src/app/components/CountRow.tsx
+++ src/app/components/CountRow.tsx
@@ -6,10 +6,26 @@ type Props = {
   count: number;
 };
 
+const typeToIcon = {
+  SLICE: "icon--", //TODO
+  FRAME: "icon--frame",
+  GROUP: "icon--group",
+  COMPONENT: "icon--component",
+  INSTANCE: "icon--instance",
+  BOOLEAN_OPERATION: "icon--",
+  VECTOR: "icon--vector-handles", // close enough icon
+  STAR: "icon--star-off",
+  LINE: "icon--minus", // close enough icon
+  ELLIPSE: "icon--", // TODO
+  POLYGON: "icon--", // TODO
+  RECTANGLE: "icon--", // TODO
+  TEXT: "icon--type"
+};
+
 const CountRow = ({ type, count, ...otherProps }: Props) => {
   return (
     <div className="count-row" {...otherProps}>
-      <div className="icon icon--adjust"></div>
+      <div className={`count-icon icon ${typeToIcon[type]}`}></div>
       <p className="count-row-type type type--pos-medium-normal">
         {macroCaseToTitleCase(type)}
       </p>
