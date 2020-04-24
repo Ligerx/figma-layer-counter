29058f333feb4714f79dc107ad2a36d706044945
Alex Wang <ipoopfool@gmail.com>
2020-04-24 10:50:30 -0700

    build second function to count all including children.

--- src/plugin/layerCounter.ts
+++ src/plugin/layerCounter.ts
@@ -2,29 +2,12 @@
 // https://www.figma.com/plugin-docs/api/nodes/
 type SceneNodeType = Exclude<NodeType, "DOCUMENT" | "PAGE">;
 
-// const nodeTypes: SceneNodeType[] = [
-//   "FRAME",
-//   "GROUP",
-//   "SLICE",
-//   "RECTANGLE",
-//   "LINE",
-//   "ELLIPSE",
-//   "POLYGON",
-//   "STAR",
-//   "VECTOR",
-//   "TEXT",
-//   "COMPONENT",
-//   "INSTANCE",
-//   "BOOLEAN_OPERATION",
-// ];
-
 type TypeCounts = { [type in SceneNodeType]?: number };
 
-// TODO: might want a toggle for "Include all nested layers"
 /**
- *
+ * Counts the number of layer types of the given nodes.
  * @param nodes
- * @returns
+ * @returns TypeCounts object
  */
 export function countLayerTypesForNodes(nodes: SceneNode[]): TypeCounts {
   const obj: TypeCounts = {};
@@ -38,19 +21,63 @@ export function countLayerTypesForNodes(nodes: SceneNode[]): TypeCounts {
 }
 
 /**
+ * Counts the number of layer types of the given nodes. This includes all children of nodes.
+ * @param nodes
+ * @returns TypeCounts object
+ */
+export function countLayerTypesForNodesAndChildren(
+  nodes: SceneNode[]
+): TypeCounts {
+  const allNodes = nodes.flatMap(getNodeAndAllChildren);
+  const obj: TypeCounts = {};
+
+  const layerTypeCounts = allNodes.reduce((accumulator, node) => {
+    accumulator[node.type] = (accumulator[node.type] ?? 0) + 1;
+    return accumulator;
+  }, obj);
+
+  return layerTypeCounts;
+}
+
+function getNodeAndAllChildren(node: SceneNode): SceneNode[] {
+  if (supportsChildren(node)) {
+    const children = node.findAll(() => true);
+    return [node, ...children];
+  }
+  return [node];
+}
+
+/**
+ * Type guard that checks if a node supports children.
+ * @param node The node to check if it supports children.
+ * @returns If `node` supports children or not.
+ */
+function supportsChildren(
+  node: SceneNode
+): node is FrameNode | ComponentNode | InstanceNode | BooleanOperationNode {
+  return (
+    node.type === "FRAME" ||
+    node.type === "GROUP" ||
+    node.type === "COMPONENT" ||
+    node.type === "INSTANCE" ||
+    node.type === "BOOLEAN_OPERATION"
+  );
+}
+
+/**
  * Takes a macro case formatted string and returns a title case formatted string.
  *
  * eg. `BOOLEAN_OPERATION` becomes `Boolean Operation`.
  * @param string
  * @returns Title case string
  */
-// function macroCaseToTitleCase(string: string): string {
-//   return string
-//       .split("_")
-//       .map(capitalizeFirstLetter)
-//       .join(" ");
-// }
+export function macroCaseToTitleCase(string: string): string {
+  return string
+    .split("_")
+    .map(capitalizeFirstLetter)
+    .join(" ");
+}
 
-// function capitalizeFirstLetter(string: string): string {
-//   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-// }
+function capitalizeFirstLetter(string: string): string {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
