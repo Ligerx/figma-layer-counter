2289164372516f923b2bfbee93fa7f4bbe7a2e28
Alex Wang <alex.wang@APPLYBITEFIFTY.workdayinternal.com>
2020-04-23 17:28:48 -0700

    remove most App boilerplate

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,48 +1,18 @@
-import * as React from 'react';
-import '../styles/ui.css';
-
-declare function require(path: string): any;
+import * as React from "react";
+import "../styles/figma-plugin-ds.min.css";
+import "../styles/ui.css";
 
 const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
+  React.useEffect(() => {
+    window.onmessage = event => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === "create-rectangles") {
+        console.log(`Figma Says: ${message}`);
+      }
+    };
+  }, []);
 
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
-
-    const onCreate = React.useCallback(() => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    }, []);
-
-    const onCancel = React.useCallback(() => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    }, []);
-
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const { type, message } = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            };
-        }
-    }, []);
-
-    return (
-        <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
-        </div>
-    );
+  return <div></div>;
 };
 
 export default App;
